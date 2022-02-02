5f3bc3e8329b47504b5ea065c87c024bab2964cf
ika17s <86973632+ika17s@users.noreply.github.com>
2022-02-02 20:51:34 +0400

    main

---
+++ js/scripts.js
@@ -0,0 +1,98 @@
+var jet = document.getElementById("jet");
+var board = document.getElementById("board");
+
+window.addEventListener("keydown", (e) => {
+  var left = parseInt(window.getComputedStyle(jet).getPropertyValue("left"));
+  if (e.key == "ArrowLeft" && left > 0) {
+    jet.style.left = left - 10 + "px";
+  }
+  //460  =>  board width - jet width
+  else if (e.key == "ArrowRight" && left <= 1000) {
+    jet.style.left = left + 10 + "px";
+  };
+  
+  if (e.keyCode == 32) {
+    //32 is for space key
+    var bullet = document.createElement("div");
+    bullet.classList.add("bullets");
+    board.appendChild(bullet);
+
+    var movebullet = setInterval(() => {
+      var rocks = document.getElementsByClassName("rocks");
+
+      for (var i = 0; i < rocks.length; i++) {
+        var rock = rocks[i];
+        if (rock != undefined) {
+          var rockbound = rock.getBoundingClientRect();
+          var bulletbound = bullet.getBoundingClientRect();
+
+          //Condition to check whether the rock/alien and the bullet are at the same position..!
+          //If so,then we have to destroy that rock
+
+          if (
+            bulletbound.left >= rockbound.left &&
+            bulletbound.right <= rockbound.right &&
+            bulletbound.top <= rockbound.top &&
+            bulletbound.bottom <= rockbound.bottom
+          ) {
+            rock.parentElement.removeChild(rock); //Just removing that particular rock;
+            //Scoreboard
+            document.getElementById("points").innerHTML =
+              parseInt(document.getElementById("points").innerHTML) + 1;
+          }
+        }
+      }
+      var bulletbottom = parseInt(
+        window.getComputedStyle(bullet).getPropertyValue("bottom")
+      );
+
+      //Stops the bullet from moving outside the gamebox
+      if (bulletbottom >= 1000) {
+        clearInterval(movebullet);
+      }
+
+      bullet.style.left = left + "px"; //bullet should always be placed at the top of my jet..!
+      bullet.style.bottom = bulletbottom + 3 + "px";
+    });
+  }
+});
+
+var generaterocks = setInterval(() => {
+  var rock = document.createElement("div");
+  rock.classList.add("rocks");
+  var rockleft = parseInt(
+    window.getComputedStyle(rock).getPropertyValue("left")
+  );
+  rock.style.left = Math.floor(Math.random() * 500) + "px";
+
+  board.appendChild(rock);
+}, 1000);
+
+var moverocks = setInterval(() => {
+  var rocks = document.getElementsByClassName("rocks");
+
+  if (rocks != undefined) {
+    for (var i = 0; i < rocks.length; i++) {
+      var rock = rocks[i];
+      var rocktop = parseInt(
+        window.getComputedStyle(rock).getPropertyValue("top")
+      );
+      if (rocktop >= 650) {
+        alert("Game Over");
+        clearInterval(moverocks);
+        window.location.reload();
+      }
+
+      rock.style.top = rocktop + 17 + "px";
+    }
+  }
+}, 500);
+
+function pause(){
+      window.alert("Press 'OK' to resume")
+}
+
+window.onload = function() {
+  window.alert("Press 'OK' to START")
+
+};
